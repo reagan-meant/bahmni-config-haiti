794f334cb77d3939343dc5c7358f431c0e9a555b
reagan-meant <reaganmeant@gmail.com>
2020-09-17 12:24:06 +0300

    HSC-24: Enhanced the new vaccination widget

--- configuration/openmrs/apps/customDisplayControl/js/customControl.js
+++ configuration/openmrs/apps/customDisplayControl/js/customControl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('bahmni.common.displaycontrol.custom')
-    .directive('immunization', ['observationsService', 'appService', 'spinner', '$q', function (observationsService, appService, spinner, $q) {
+    .directive('immunization', ['observationsService','appService', 'spinner', '$q', function (observationsService, appService, spinner, $q) {
         var link = function ($scope) {
 
             $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/immunization.html";
@@ -221,4 +221,29 @@ angular.module('bahmni.common.displaycontrol.custom')
                 $scope.reverse = !$scope.reverse;
             };
 
-        }]);
+        }]).filter("unique", function() {
+            // we will return a function which will take in a collection
+            // and a keyname
+            return function(collection, keyname) {
+              // we define our output and keys array;
+              var output = [],
+                keys = [];
+          
+              // we utilize angular's foreach function
+              // this takes in our original collection and an iterator function
+              angular.forEach(collection, function(item) {
+                // we check to see whether our object exists
+                var key = item[keyname];
+                // if it's not already part of our keys array
+                if (keys.indexOf(key) === -1) {
+                  // add it to our keys array
+                  keys.push(key);
+                  // push this item to our final output array
+                  output.push(item);
+                }
+              });
+              // return our array which should be devoid of
+              // any duplicates
+              return output;
+            };
+          });
